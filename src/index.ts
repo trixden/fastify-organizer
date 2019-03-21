97004c926d23ed60031e0ac352e60fdc637587ee
Denis Karimov <trixden@gmail.com>
2019-03-21 20:10:36 +0300

    * | fix typescript dynamic imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import glob from 'glob';
+import glob from 'fast-glob';
 import * as fastify from 'fastify';
 import fp from 'fastify-plugin';
 import {Server, IncomingMessage, ServerResponse} from 'http';
@@ -11,57 +11,55 @@ interface OrganizerOpts {
   prodDir?: string
 };
 
-const fastifyOrganizer: fastify.Plugin<Server, IncomingMessage, ServerResponse, OrganizerOpts> = (fastify, opts, next) => {
+const fastifyOrganizer: fastify.Plugin<Server, IncomingMessage, ServerResponse, OrganizerOpts> = async (fastify, opts) => {
   const directory = (process.env.NODE_ENV !== 'production' || !opts.prodDir) ? opts.dir : opts.prodDir;
 
-  glob(path.join(directory, '/**/*.{js,ts}'), (error, files) => {
-    if (error) throw error;
+  const entries = await glob(path.join(directory, '/**/*.{js,ts}'));
 
-    for (let file of files) {
-      if (opts.ignorePattern && opts.ignorePattern.test(file)) return;
+  for (let entry of entries) {
+    const file = typeof entry === 'string' ? {path: entry} : {path: entry.path};
 
-      const plugin = require(file);
+    if (opts.ignorePattern && opts.ignorePattern.test(file.path)) return;
 
-      if (plugin.autoload !== undefined && plugin.autoload !== true) continue;
+    const plugin = await import(file.path);
 
-      switch (opts.type) {
-        case 'routes':
-          fastify.route(plugin.default(fastify));
-          break;
-        case 'decorators':
-          if (plugin.target === 'request') {
-            fastify.decorateRequest(plugin.name, plugin.default);
-          }
-          if (plugin.target === 'reply') {
-            fastify.decorateReply(plugin.name, plugin.default);
-          }
-          if (!plugin.target) {
-            fastify.decorate(plugin.name, plugin.default);
-          }
-          break;
-        case 'middlewares':
-          if (plugin.url) {
-            fastify.use(plugin.url, plugin.default);
-          } else {
-            fastify.use(plugin.default);
-          }
-          break;
-        case 'hooks':
-          fastify.addHook(plugin.event, plugin.default);
-          break;
-        case 'plugins':
-          fastify.register(plugin.default, plugin.opts);
-          break;
-        case 'parsers':
-          fastify.addContentTypeParser(plugin.type, plugin.opts);
-          break;
-        default:
-          break;
-      }
+    if (plugin.autoload !== undefined && plugin.autoload !== true) continue;
+
+    switch (opts.type) {
+      case 'routes':
+        fastify.route(plugin.default(fastify));
+        break;
+      case 'decorators':
+        if (plugin.target === 'request') {
+          fastify.decorateRequest(plugin.name, plugin.default);
+        }
+        if (plugin.target === 'reply') {
+          fastify.decorateReply(plugin.name, plugin.default);
+        }
+        if (!plugin.target) {
+          fastify.decorate(plugin.name, plugin.default);
+        }
+        break;
+      case 'middlewares':
+        if (plugin.url) {
+          fastify.use(plugin.url, plugin.default);
+        } else {
+          fastify.use(plugin.default);
+        }
+        break;
+      case 'hooks':
+        fastify.addHook(plugin.event, plugin.default);
+        break;
+      case 'plugins':
+        fastify.register(plugin.default, plugin.opts);
+        break;
+      case 'parsers':
+        fastify.addContentTypeParser(plugin.type, plugin.opts);
+        break;
+      default:
+        break;
     }
-
-    next();
-  });
+  }
 };
 
 export = fp(fastifyOrganizer);
